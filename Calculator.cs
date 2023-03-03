f509d56ee4e72b1a5c6f9852550e604f441ec8c6
Kimia Rostami <kimiarostami@hotmail.com>
2023-03-03 15:47:25 +0100

    initial commit

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+namespace Test01
+{
+    class Calculator
+    {
+        public static int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+        public static int Sub(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+        public static int Mult(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+        public static int Div(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Test01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum, sub, mult, div;
+
+            sum = Calculator.Add(2, 8);
+            sub = Calculator.Sub(6, sum);
+            Console.WriteLine($"6 - {sum} = {sub}");
+            mult = Calculator.Mult(sub, 2);
+            Console.WriteLine(mult);
+            div = Calculator.Div(sub, 2);
+            Console.WriteLine(div);
+            RunGreeter();
+        }
+
+
+        static void RunGreeter()
+        {
+            Console.WriteLine("Whats your name?");
+            string name = Console.ReadLine();
+            Greeter(name);
+
+        }
+        static void Greeter(string name)
+        {
+            Console.WriteLine("Hello " + name + ", how are you?");
+        }
+    }
+}
